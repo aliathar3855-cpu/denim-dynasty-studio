6ae719d3c20c55ab86bbaae226365f27f9b4c140
aliathar3855-cpu <aliathar3855@gmail.com>
2026-05-30 15:17:06 +0530

    fix product page

--- src/app/products/id/page.tsx
+++ src/app/products/id/page.tsx
@@ -1,30 +1,22 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import { useParams } from "next/navigation";
-
-import {
-  doc,
-  getDoc,
-} from "firebase/firestore";
-
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
+import { useCart } from "@/context/CartContext";
 
 export default function ProductPage() {
-
   const { id } = useParams();
 
   const [product, setProduct] = useState<any>(null);
-
   const [loading, setLoading] = useState(true);
 
+  const { addToCart } = useCart();
+
   useEffect(() => {
-
     const fetchProduct = async () => {
-
       try {
-
         if (!id) return;
 
         const docRef = doc(
@@ -36,32 +28,22 @@ export default function ProductPage() {
         const snapshot = await getDoc(docRef);
 
         if (snapshot.exists()) {
-
           setProduct({
             id: snapshot.id,
             ...snapshot.data(),
           });
-
         }
-
       } catch (error) {
-
         console.error(error);
-
       } finally {
-
         setLoading(false);
-
       }
-
     };
 
     fetchProduct();
-
   }, [id]);
 
   if (loading) {
-
     return (
       <main className="min-h-screen bg-black text-white flex items-center justify-center">
         <h1 className="text-3xl font-bold">
@@ -69,11 +51,9 @@ export default function ProductPage() {
         </h1>
       </main>
     );
-
   }
 
   if (!product) {
-
     return (
       <main className="min-h-screen bg-black text-white flex items-center justify-center">
         <h1 className="text-3xl font-bold">
@@ -81,7 +61,6 @@ export default function ProductPage() {
         </h1>
       </main>
     );
-
   }
 
   return (
@@ -90,8 +69,38 @@ export default function ProductPage() {
       <div className="grid md:grid-cols-2 gap-10">
 
         <div>
-
           <img
             src={product.imageUrl}
             alt={product.name}
-            className="
+            className="w-full h-[500px] object-cover rounded-3xl"
+          />
+        </div>
+
+        <div className="flex flex-col justify-center">
+
+          <h1 className="text-5xl font-bold">
+            {product.name}
+          </h1>
+
+          <p className="text-3xl font-semibold mt-6">
+            ₹{product.price}
+          </p>
+
+          <p className="text-gray-400 mt-6 text-lg">
+            {product.description}
+          </p>
+
+          <button
+            onClick={() => addToCart(product)}
+            className="mt-8 bg-white text-black py-4 px-8 rounded-xl font-bold hover:bg-gray-200 transition"
+          >
+            Add To Cart
+          </button>
+
+        </div>
+
+      </div>
+
+    </main>
+  );
+}
